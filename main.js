fb94b29edbd52fd56999bb4e6fc03c34223e8fdd
TinaBaldwin <122581994+TinaBaldwin@users.noreply.github.com>
2023-03-05 16:43:12 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log ('javascript loaded');
+
+
+const hamburger - document.querySelector('.hamburger');
+const navigation-document.querySelector('navigation');
+
+
+
+ /**  hamburger.addEventlistener('click', function(){
+    hamburger.classList.toggle('menu-open');
+    navigation.classList.toggle('')
+  });
+**/
