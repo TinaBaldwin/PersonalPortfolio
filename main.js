5bc4ad6ac1196264583caefcf7b06b61a69a968d
TinaBaldwin <OTBaldwin@gmail.com>
2023-03-23 20:52:36 -0400

    JS array for skills list

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("open")
     nav.classList.toggle('navbar')
 });
+
+
+let list = document.getElementById("skillsList");
+
+let data = ['HTML', 'CSS', 'Javascript'];
+
+data.forEach((item)=>{
+    let li = document.createElement("li");
+    console.log(li);
+    li.innerText = item;
+    list.appendChild(li);
+})
