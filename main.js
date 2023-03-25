ff51610f0b6b8e001bcb6fb6c5d3269fbe28dc36
TinaBaldwin <OTBaldwin@gmail.com>
2023-03-24 21:01:11 -0400

    updates with both arr and hamb working

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ hamburger.addEventListener("click", function () {
 
 
 
-let list = document.getElementById("skillsList");
-
 let data = ['HTML', 'CSS', 'Javascript'];
+
+let list = document.getElementById("skillsList");
 
 data.forEach((item)=>{
     let li = document.createElement("li");
     li.innerText = item;
+    console.log(list);
     list.appendChild(li);
 });
