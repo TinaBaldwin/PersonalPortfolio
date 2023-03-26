88353c1b5cd9915fddf307587cbcf7ec32de58d9
TinaBaldwin <OTBaldwin@gmail.com>
2023-03-25 20:35:03 -0400

    attempt to add accordion

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ hamburger.addEventListener("click", function () {
 
 
 
-
 let data = ['HTML', 'CSS', 'Javascript'];
 
 let list = document.getElementById("skillsList");
@@ -21,3 +20,19 @@ data.forEach((item)=>{
     console.log(list);
     list.appendChild(li);
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+     this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+    });
+}
